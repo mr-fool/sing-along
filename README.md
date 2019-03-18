9b7e7c2bab58eeaec6bfb33f7ecebc5a43b46782
mr-fool <mr-fool@users.noreply.github.com>
2019-03-17 18:44:51 -0600

    initial file

---
+++ README.md
@@ -0,0 +1,11 @@
+# Challenge - Sing-along
+
+## The Task
+
+1. Parse the provided String (`song`) and create an array with
+   an entry for each line of the song.
+   Eg: (`["Baby Shark doo doo doo doo doo doo", "Baby Shark!"]`)
+2. Render each line of that array to the DOM as an `<li>`
+   within the `#lyrics-list` unordered list.
+3. Display each array item to the page sequentially, at
+   1.5 second intervals
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+const song = `Baby Shark doo doo doo doo doo doo
+Baby Shark doo doo doo doo doo doo
+Baby Shark doo doo doo doo doo doo
+Baby Shark!
+Daddy Shark doo doo doo doo doo doo
+Daddy Shark doo doo doo doo doo doo
+Daddy Shark doo doo doo doo doo doo
+Daddy Shark!
+Mommy Shark doo doo doo doo doo doo
+Mommy Shark doo doo doo doo doo doo
+Mommy Shark doo doo doo doo doo doo
+Mommy Shark!
+Grandpa Shark doo doo doo doo doo doo
+Grandpa Shark doo doo doo doo doo doo
+Grandpa Shark doo doo doo doo doo doo
+Grandpa Shark!
+Grandma Shark doo doo doo doo doo doo
+Grandma Shark doo doo doo doo doo doo
+Grandma Shark doo doo doo doo doo doo
+Grandma Shark!`;
